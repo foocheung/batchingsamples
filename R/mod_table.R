e09ee264bcac485c71904b8a44aa0c07805110d9
foocheung <foo.cheung@nih.gov>
2023-01-08 13:17:10 -0500

    Update mod_table.R

--- R/mod_table.R
+++ R/mod_table.R
@@ -84,23 +84,21 @@ mod_table_server <- function(input, output, session, file){  #,batches,sim){
  # output$text2 <- renderTable({
   sliderValues <- reactive({
 
-   # batches<-input$integer
-  #  dffs<<-file()$sim
-    dff<<-file$df()
-   # aaa<<-read.table("inst/app/www/input_ex_adat.txt", header=T, fill=TRUE, sep="\t")
+      dff<-file$df()
+  
     sbj_per_batch<-ceiling(nrow(dff)/file$integer())
 
-    myChip <<- new("BeadChip", nRows=sbj_per_batch, nColumns=1, byrow=FALSE, comment="mock chip")
-    myPlate<<-new("BeadPlate", chip=myChip,nRows=1L, nColumns=1L)
+    myChip <- new("BeadChip", nRows=sbj_per_batch, nColumns=1, byrow=FALSE, comment="mock chip")
+    myPlate<-new("BeadPlate", chip=myChip,nRows=1L, nColumns=1L)
     cn<-colnames(dff)
-    cnum <<- ncol(dff)
-    gss <<- OSAT::setup.sample(dff, optimal=c(optimal=cn[-1]) )
+    cnum <- ncol(dff)
+    gss <- OSAT::setup.sample(dff, optimal=c(optimal=cn[-1]) )
 
-    gcc <<- OSAT::setup.container(myPlate, file$integer(), batch="plates")
-    gsetup <<- OSAT::create.optimized.setup(sample=gss, container=gcc, nSim=file$sim())
-    results<<-as.matrix(capture.output(OSAT::QC(gsetup)))
-    tab<<-OSAT::get.experiment.setup(gsetup) %>% dplyr::select(0:cnum+1)
-    tab<<-dplyr::rename(tab,  Batch = plates)
+    gcc <- OSAT::setup.container(myPlate, file$integer(), batch="plates")
+    gsetup <- OSAT::create.optimized.setup(sample=gss, container=gcc, nSim=file$sim())
+    results<-as.matrix(capture.output(OSAT::QC(gsetup)))
+    tab<-OSAT::get.experiment.setup(gsetup) %>% dplyr::select(0:cnum+1)
+    tab<-dplyr::rename(tab,  Batch = plates)
     return(list("gsetup"=gsetup,"tab"=tab, "results"=results))
     #results
   })
